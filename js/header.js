8899b4a91c29ae83b3353131dde47f5661613fb0
disha seth <dishaseth342@gmail.com>
2024-11-16 03:40:35 +0530

    website ui done

--- js/header.js
+++ js/header.js
@@ -5,19 +5,22 @@ let appHeader = `
         <!-- Navigation Bar -->
         <div class="navbar max-sm:py-6 flex items-center justify-between px-6 md:px-16 md:py-5">
             <!-- Logo -->
-            <div>
-                <img class="h-8" src="./AaharKosh_logo.jpg" alt="AaharKosh Logo">
-            </div>
+            <a href="./index.html#app-header"" class="flex items-center justify-center gap-4" >
+                <div class="rounded-xl p-1"  style="box-shadow: 0 0 10px 2px rgba(0, 0, 0, 0.15);">
+                    <img class="h-[3.5vw] max-sm:h-[10vw]" src="./images/aaharkosh-logo.jpg" alt="AaharKosh Logo">
+                </div>
+                <img class="h-[2.5vw] max-sm:h-[7.5vw]" src="./images/aaharkosh-text-image.svg" alt="AaharKosh Logo">
+            </a>
         
             <!-- Navigation Links (Desktop) -->
             <div>
-                <ul class="hidden md:flex space-x-6 md:space-x-16 font-medium text-lg mt-2">
+                <ul class="hidden md:flex space-x-6 md:space-x-16 font-medium text-lg ">
                     <li><a href="./index.html#app-header" class="relative after:content-[''] after:absolute after:w-full after:h-[1px] after:bg-orange-600 after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left hover:text-orange-600 transition-all">Home</a></li>
                     <li><a href="./index.html#plansPage" class="relative after:content-[''] after:absolute after:w-full after:h-[1px] after:bg-orange-600 after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left hover:text-orange-600 transition-all ml-[-1.5vw] max-lg:ml-[-5vw]">Plans</a></li>
                     <li><a href="./index.html#servicePage" class="relative after:content-[''] after:absolute after:w-full after:h-[1px] after:bg-orange-600 after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left hover:text-orange-600 transition-all ml-[-1.5vw] max-lg:ml-[-5vw]">How it Works</a></li>
                     <li><a href="./index.html#why-choose-us" class="relative after:content-[''] after:absolute after:w-full after:h-[1px] after:bg-orange-600 after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left hover:text-orange-600 transition-all ml-[-1.5vw] max-lg:ml-[-5vw]">Why Us</a></li>
                     <li><a href="./index.html#testimonials" class="relative after:content-[''] after:absolute after:w-full after:h-[1px] after:bg-orange-600 after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left hover:text-orange-600 transition-all ml-[-1.5vw] max-lg:ml-[-5vw]">Testimonial</a></li>
-                    <li><a href="./index.html#aboutPage" class="relative after:content-[''] after:absolute after:w-full after:h-[1px] after:bg-orange-600 after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left hover:text-orange-600 transition-all ml-[-1.5vw] max-lg:ml-[-5vw]">About Us</a></li>
+                    <li><a href="./aboutus.html" class="relative after:content-[''] after:absolute after:w-full after:h-[1px] after:bg-orange-600 after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left hover:text-orange-600 transition-all ml-[-1.5vw] max-lg:ml-[-5vw]">About Us</a></li>
                     <li><a href="./index.html#contactPage" class="relative after:content-[''] after:absolute after:w-full after:h-[1px] after:bg-orange-600 after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left hover:text-orange-600 transition-all ml-[-1.5vw] max-lg:ml-[-5vw]">Contacts</a></li>
                 </ul>
             </div>
