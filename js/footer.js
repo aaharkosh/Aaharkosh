17f18b28919b430b36281d99743a66b5462285f8
disha seth <dishaseth342@gmail.com>
2025-01-20 00:08:49 +0530

    contentchanged

--- js/footer.js
+++ js/footer.js
@@ -8,9 +8,9 @@ let appfooter = `
                 <!-- First Column -->
                 <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4 md:p-6">
                     <img class="h-[2.5vw] max-sm:h-[7.5vw]" src="./images/aaharkosh-text-image.svg" alt="AaharKosh Logo">
-                    <div class="flex gap-3 mt-5">
+                    <div class="flex gap-3 mt-5 text-black">
                         <button class="text-lg px-2 py-1 shadow-md rounded-xl cursor-pointer bg-white transition duration-300 ease-in-out hover:scale-110">
-                            <i class="ri-twitter-fill"></i>
+                            <i class="ri-youtube-fill"></i>
                         </button>
                         <button class="text-lg px-2 py-1 shadow-md rounded-xl cursor-pointer bg-white transition duration-300 ease-in-out hover:scale-110">
                             <i class="ri-instagram-fill"></i>
@@ -34,7 +34,7 @@ let appfooter = `
                 <!-- third column -->
                 <div class="md:w-1/2 xl:w-1/4 lg:w-1/2 md:p-6 max-md:ml-4 max-sm:mt-10 max-md:mt-4 flex flex-col">
                     <h4 class="font-semibold text-lg max-md:text-lg max-sm:text-xl max-lg:text-3xl">Partner with us</h4>
-                    <a href="./vender.html" class="text-md max-sm:text-[4.2vw] mt-2 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-[9vw] after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Become our vender</a>
+                    <a href="./vender.html" class="text-md max-sm:text-[4.2vw] mt-2 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-[9vw] after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Become our vendor</a>
                     <a href="./rider.html" class="text-md max-sm:text-[4.2vw] mt-1 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-32 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Become our rider</a>
                 </div>
                 <!-- Fourth Column -->
@@ -43,6 +43,11 @@ let appfooter = `
                     <a href="./feedback.html" class="text-md max-sm:text-[4.2vw] mt-1 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-[4.5vw] after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Feedback</a>
                     <a href="./index.html#contactPage" class="text-md max-sm:text-[4.2vw] max-sm:text-md mt-1 max-lg:text-xl cursor-pointer relative after:content-[''] after:absolute after:w-16 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Contacts</a>
                 </div>
+                <div class="md:w-1/2 xl:w-1/4 lg:w-1/2 p-4 md:p-6 max-md:mt-4 flex max-sm:mt-8 flex-col">
+                    <h4 class="font-semibold text-lg max-md:text-lg max-sm:text-xl max-lg:text-3xl">Legal</h4>
+                    <a href="./privacy.html" class="text-md max-sm:text-[4.2vw] mt-1 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-[6.5vw] after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Privacy Policy</a>
+                    <a href="./terms.html" class="text-md max-sm:text-[4.2vw] max-sm:text-md mt-1 max-lg:text-xl cursor-pointer relative after:content-[''] after:absolute after:w-36 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Terms & Conditions</a>
+                </div>
                 <!-- Fifth Column -->
              <div class="md:w-1/2 xl:w-1/4 lg:w-1/2 p-4 md:p-6 max-md:mt-4 flex flex-col items-start"> 
                     <h4 class="font-semibold text-lg max-md:text-lg max-sm:text-xl max-lg:text-3xl">Stay Connected</h4>
@@ -50,10 +55,7 @@ let appfooter = `
                         <i class="ri-phone-line text-2xl"></i>
                         <div class="text-md max-sm:text-[4.2vw] max-sm:text-lg flex flex-col">
                             <a class="cursor-pointer hover:text-[#FF570C] transition-all duration-300">
-                                +91 6521312315
-                            </a>
-                            <a class="cursor-pointer hover:text-[#FF570C] transition-all duration-300">
-                                +91 6542312315
+                                +91 7987681268
                             </a>
                         </div>
                     </div>
@@ -74,9 +76,6 @@ let appfooter = `
             </div>
             <!-- Footer Links -->
             <div class="flex flex-wrap justify-center w-full items-center gap-12 text-sm border-t-[1px] border-zinc-700 mt-12">
-                <a href="./policy.html" class="mt-4 cursor-pointer relative after:content-[''] after:absolute after:w-20 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Privacy Policy</a>
-                <a href="./terms.html" class="mt-4 cursor-pointer relative after:content-[''] after:absolute after:w-32 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Terms & Conditions</a>
-                <h6 class="mt-4 max-md:mt-[-10vw] cursor-pointer relative after:content-[''] after:absolute after:w-32 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Opt-out Preferences</h6>
             </div>
             <!-- Copyright -->
             <div class="text-center text-xs mt-5">
