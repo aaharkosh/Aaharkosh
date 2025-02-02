6e49c731443f7fbdeb8ade8722f7f0580c529bf6
disha seth <dishaseth342@gmail.com>
2025-02-02 20:26:17 +0530

    tailwind added

---
+++ js/footer.js
@@ -0,0 +1,82 @@
+let appfooter = `
+    
+    <!-- FOOTER-SECTION -->
+
+    <footer class="bg-[var(--color-primary-light)] w-full overflow-hidden">
+        <div class=" mx-auto py-6 px-16 pt-6 max-md:p-6 max-lg:p-12 footer-content">
+            <div class="flex max-md:flex-col max-lg:flex-col justify-between gap-8">
+                <!-- First Column -->
+                <div class="">
+                    <img class="h-[2.5vw] max-sm:h-[7.5vw]" src="/images/aaharkosh-text-image.svg" alt="AaharKosh Logo">
+                    <div class="flex gap-3 mt-5 text-black">
+                        <button class="text-lg px-2 py-1 shadow-md rounded-xl cursor-pointer bg-white transition duration-300 ease-in-out hover:scale-110">
+                            <i class="ri-youtube-fill"></i>
+                        </button>
+                        <button class="text-lg px-2 py-1 shadow-md rounded-xl cursor-pointer bg-white transition duration-300 ease-in-out hover:scale-110">
+                            <i class="ri-instagram-fill"></i>
+                        </button>
+                        <button class="text-lg px-2 py-1 shadow-md rounded-xl cursor-pointer bg-white transition duration-300 ease-in-out hover:scale-110">
+                            <i class="ri-facebook-fill"></i>
+                        </button>
+                        <button class="text-lg px-2 py-1 shadow-md rounded-xl cursor-pointer bg-white transition duration-300 ease-in-out hover:scale-110">
+                            <i class="ri-google-fill"></i>
+                        </button>
+                    </div>
+                </div>
+                <!-- Second Column -->
+                <div class="flex flex-col">
+                    <h4 class="font-semibold text-lg max-md:text-lg max-sm:text-xl max-lg:text-3xl">Company</h4>
+                    <a href="./index.html#why-choose-us" class="text-md max-sm:text-[4.2vw] max-lg:text-xl max-md:text-md max-lg:mt-4 mt-2 max-sm:mt-2 cursor-pointer relative after:content-[''] after:absolute after:w-28 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Why Aaharkosh?</a>
+                    <a href="./whatwedo.html" class="text-md max-sm:text-[4.2vw] max-lg:text-xl max-md:text-md max-sm:mt-2 mt-1 cursor-pointer relative after:content-[''] after:absolute after:w-20 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">What we do</a>
+                    <a href="./aboutus.html" class="text-md max-sm:text-[4.2vw] mt-1 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-[4.5vw] after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">About us</a>
+                    <a href="./faqs.html" class="text-md max-sm:text-[4.2vw] mt-1 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-10 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">FAQs</a>
+                </div>
+                <!-- third column -->
+                <div class=" flex flex-col">
+                    <h4 class="font-semibold text-lg max-md:text-lg max-sm:text-xl max-lg:text-3xl">Partner with us</h4>
+                    <a href="./vender.html" class="text-md max-sm:text-[4.2vw] mt-2 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-[9vw] after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Become our vendor</a>
+                    <a href="./rider.html" class="text-md max-sm:text-[4.2vw] mt-1 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-32 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Become our rider</a>
+                </div>
+                <!-- Fourth Column -->
+                <div class="flex-col flex">
+                    <h4 class="font-semibold text-lg max-md:text-lg max-sm:text-xl max-lg:text-3xl">Support</h4>
+                    <a href="./feedback.html" class="text-md max-sm:text-[4.2vw] mt-1 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-[4.5vw] after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Feedback</a>
+                    <a href="./index.html#contactPage" class="text-md max-sm:text-[4.2vw] max-sm:text-md mt-1 max-lg:text-xl cursor-pointer relative after:content-[''] after:absolute after:w-16 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Contacts</a>
+                </div>
+                <!-- fifth coloumn -->
+                <div class="flex-col flex">
+                    <h4 class="font-semibold text-lg max-md:text-lg max-sm:text-xl max-lg:text-3xl">Legal</h4>
+                    <a href="./policy.html" class="text-md max-sm:text-[4.2vw] mt-1 max-lg:text-xl max-md:text-md cursor-pointer relative after:content-[''] after:absolute after:w-[6.5vw] after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Privacy Policy</a>
+                    <a href="./terms.html" class="text-md max-sm:text-[4.2vw] max-sm:text-md mt-1 max-lg:text-xl cursor-pointer relative after:content-[''] after:absolute after:w-36 after:h-[0.05vw] after:bg-black after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all">Terms & Conditions</a>
+                </div>
+                <!-- sixth Column -->
+             <div class="flex flex-col items-start"> 
+                    <h4 class="font-semibold text-lg max-md:text-lg max-sm:text-xl max-lg:text-3xl">Stay Connected</h4>
+                    <div class="flex text-md max-sm:text-[4.2vw] items-center gap-4 mt-2 justify-center cursor-pointer hover:text-[#FF570C] transition-all duration-300">
+                        <i class="ri-phone-line text-2xl"></i>
+                        <div class="text-md max-sm:text-[4.2vw] flex flex-col">
+                            <a class="cursor-pointer">
+                                +91 7987681268
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex text-md max-sm:text-[4.2vw] items-center gap-4 justify-center cursor-pointer hover:text-[#FF570C] transition-all duration-300">
+                        <i class="ri-mail-line text-2xl"></i>
+                        <a class="text-md max-sm:text-[4.2vw]">
+                            aaharkosh@gmail.com
+                        </a>
+                    </div>
+                </div> 
+            </div>
+            <!-- Footer Links -->
+            <div class="flex flex-wrap justify-center w-full items-center gap-12 text-sm border-t-[1px] border-zinc-700 mt-12">
+            </div>
+            <!-- Copyright -->
+            <div class="text-center text-xs mt-5">
+                <h1>© 2025 Your Website. All rights reserved.</h1>
+            </div>
+        </div>
+    </footer>
+
+`;
+document.getElementById("app-footer").innerHTML = appfooter;
---
+++ js/header.js
@@ -0,0 +1,130 @@
+let appHeader = `
+
+    <!-- NAVBAR -->
+    <div id="app-header" class="fixed top-0 left-0 w-full z-50 transition-all duration-500">
+    <!-- Navigation Bar -->
+    <div class="navbar flex items-center justify-between transition-all px-4 md:px-16 py-5">
+        <!-- Logo -->
+        <a href="./index.html#app-header" class="flex items-center gap-4">
+            <div class="rounded-xl p-1">
+                <img class="h-[3.5vw] max-sm:h-[10vw] rounded-lg" style="box-shadow: 0 0 10px 2px rgba(0, 0, 0, 0.15);"  src="/images/app logo.png" alt="AaharKosh Logo">
+            </div>
+            <img class="h-[2vw] max-sm:h-[7.5vw]" src="/images/aaharkosh-text-image.svg" alt="AaharKosh Logo">
+        </a>
+    
+        <!-- Navigation Links (Desktop) -->
+        <ul class="hidden md:flex space-x-16 max-md:space-x-6 font-medium text-lg">
+            <li><a href="./index.html#app-header" class="nav-link relative cursor-pointer after:content-[''] after:absolute after:w-full hover:text-[var(--color-primary)] after:h-[0.13vw] after:bg-[var(--color-primary)] after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all hover:after:scale-x-100">Home</a></li>
+            <li><a href="./index.html#plansPage" class="nav-link relative cursor-pointer after:content-[''] after:absolute after:w-full hover:text-[var(--color-primary)] after:h-[0.13vw] after:bg-[var(--color-primary)] after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all hover:after:scale-x-100 hover:after:scale-x-100">Plans</a></li>
+            <li><a href="./index.html#servicePage" class="nav-link relative cursor-pointer after:content-[''] after:absolute after:w-full hover:text-[var(--color-primary)] after:h-[0.13vw] after:bg-[var(--color-primary)] after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all hover:after:scale-x-100 hover:after:scale-x-100">How it Works</a></li>
+            <li><a href="./index.html#why-choose-us" class="nav-link relative cursor-pointer after:content-[''] after:absolute after:w-full hover:text-[var(--color-primary)] after:h-[0.13vw] after:bg-[var(--color-primary)] after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all hover:after:scale-x-100 hover:after:scale-x-100">Why Us</a></li>
+            <li><a href="./team.html" class="nav-link relative cursor-pointer after:content-[''] after:absolute after:w-full hover:text-[var(--color-primary)] after:h-[0.13vw] after:bg-[var(--color-primary)] after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all hover:after:scale-x-100 hover:after:scale-x-100">Career</a></li>
+            <li><a href="./aboutus.html" class="nav-link relative cursor-pointer after:content-[''] after:absolute after:w-full hover:text-[var(--color-primary)] after:h-[0.13vw] after:bg-[var(--color-primary)] after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all hover:after:scale-x-100 hover:after:scale-x-100">About Us</a></li>
+            <li><a href="./index.html#contactPage" class="nav-link relative cursor-pointer after:content-[''] after:absolute after:w-full hover:text-[var(--color-primary)] after:h-[0.13vw] after:bg-[var(--color-primary)] after:left-0 after:bottom-[-5%] after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-[500ms] after:ease-out hover:after:scale-x-100 hover:after:origin-bottom-left transition-all hover:after:scale-x-100 hover:after:scale-x-100">Contacts</a></li>
+        </ul>
+    
+        <!-- Hamburger Icon (Mobile) -->
+        <div class="md:hidden flex items-center">
+            <button id="hamburger" class="text-3xl focus:outline-none">
+                <i class="ri-menu-line"></i>
+            </button>
+        </div>
+    </div>
+
+    <!-- Mobile Menu (Full-screen Overlay) -->
+    <div id="mobile-menu" class="fixed inset-0 bg-gray-100 z-50 hidden flex flex-col p-5 pt-10 space-y-4 max-md:space-y-12 max-sm:space-y-6 text-3xl font-medium transform -translate-x-full transition-transform duration-500 ease-in-out">
+        <!-- Close Icon -->
+        <div class="relative pb-12">
+            <img class="h-8 absolute top-1 left-6" src="/images/aaharkosh-text-image.svg" alt="AaharKosh Logo">
+            <button id="close-menu" class="absolute top-0 right-6 text-4xl focus:outline-none">
+                <i class="ri-close-line"></i>
+            </button>
+        </div>
+        <a href="./index.html#app-header" class="text-black h-10 flex items-center pl-4 text-[5vw]"><i class="ri-home-4-line mr-6"></i> Home</a>
+        <a href="./index.html#plansPage" class="text-black h-10 flex items-center pl-4 text-[5vw]"><i class="ri-chat-check-line mr-6"></i> Plans</a>
+        <a href="./index.html#servicePage" class="text-black h-10 flex items-center pl-4 text-[5vw]"><i class="ri-customer-service-line mr-6"></i> How it Works</a>
+        <a href="./index.html#why-choose-us" class="text-black h-10 flex items-center pl-4 text-[5vw]"><i class="ri-map-pin-user-line mr-6"></i> Why Us</a>
+        <a href="./team.html" class="text-black h-10 flex items-center pl-4 text-[5vw]"><i class="ri-customer-service-line mr-6"></i>Career</a>
+        <a href="./aboutus.html" class="text-black h-10 flex items-center pl-4 text-[5vw]"><i class="ri-group-line mr-6"></i> About Us</a>
+        <a href="./index.html#contactPage" class="text-black h-10 flex items-center pl-4 text-[5vw]"><i class="ri-phone-fill mr-6"></i> Contacts</a>
+    </div>
+</div>
+
+
+`;
+
+
+// navbar script js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+    const pageName = window.location.pathname.split("/").pop(); // Get current page name
+
+    // Define page-specific background colors
+    const pageColors = {
+        "faqs.html": "bg-[var(--color-primary-light)]",  
+        "aboutus.html": "bg-[var(--color-primary-light)]", 
+        "feedback.html": "bg-[var(--color-primary-light)]", 
+        "policy.html": "bg-[var(--color-primary-light)]", 
+        "allplans.html": "bg-[var(--color-primary-light)]",
+        "whatwedo.html": "bg-[var(--color-primary-light)]",
+        "terms.html": "bg-[var(--color-primary-light)]"
+    };
+
+    // Set default background if page matches, otherwise keep white
+    if (pageColors[pageName]) {
+        navbar.classList.add(pageColors[pageName]);
+    } else {
+        navbar.classList.add("bg-white");
+    }
+
+    // Change navbar background on scroll
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.remove(...Object.values(pageColors)); // Remove all possible colors
+            navbar.classList.add("bg-white");
+        } else {
+            if (pageColors[pageName]) {
+                navbar.classList.remove("bg-white");
+                navbar.classList.add(pageColors[pageName]);
+            }
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+document.getElementById("app-header").innerHTML = appHeader;
+const appheader = document.querySelector("#app-header")
+const hamburger = document.getElementById('hamburger');
+const closeMenu = document.getElementById('close-menu');
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileLinks = mobileMenu.querySelectorAll('a'); // Select all links inside the mobile menu
+const navbar = document.querySelector('.navbar'); // Select the navbar
+// let lastScrollY = window.scrollY; // Track the last scroll position
+
+// Toggle mobile menu
+hamburger.addEventListener('click', () => {
+    mobileMenu.classList.remove('hidden'); // Make menu visible
+    setTimeout(() => mobileMenu.classList.remove('-translate-x-full'), 10); // Smooth open transition
+});
+
+closeMenu.addEventListener('click', () => {
+    mobileMenu.classList.add('-translate-x-full'); // Start close transition
+    setTimeout(() => mobileMenu.classList.add('hidden'), 500); // Wait for animation to finish, then hide
+});
+
+// Close menu when clicking on any link
+mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.add('-translate-x-full'); // Close with animation
+        setTimeout(() => mobileMenu.classList.add('hidden'), 500); // Hide after animation
+    });
+});
